dd02679b783fafddb6ffa3cd38e04ed0e629b1a5
Babaoni147 <obahedwin@ymail.com>
2024-06-26 22:09:25 +0100

    Update

--- models/__init__.py
+++ models/__init__.py
@@ -1,22 +1,19 @@
 #!/usr/bin/python3
-"""This module instantiates an storage"""
-
-from os import getenv
+"""create a unique FileStorage instance for your application"""
+from models.engine.file_storage import FileStorage
+from models.engine.db_storage import DBStorage
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
 from models.amenity import Amenity
 from models.place import Place
-from models.base_model import BaseModel
-from models.city import City
-from models.user import User
 from models.review import Review
-from models.state import State
+from os import getenv
 
-refs_classes = {"Amenity": Amenity, "BaseModel": BaseModel, "City": City,
-           "Place": Place, "Review": Review, "State": State, "User": User}
 
 if getenv("HBNB_TYPE_STORAGE") == "db":
-    from models.engine.db_storage import DBStorage
     storage = DBStorage()
 else:
-    from models.engine.file_storage import FileStorage
     storage = FileStorage()
 storage.reload()
--- models/state.py
+++ models/state.py
@@ -1,29 +1,35 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
+"""This is the state class"""
+from sqlalchemy.ext.declarative import declarative_base
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
-from os import getenv
+from sqlalchemy import Column, Integer, String
+import models
+from models.city import City
+import shlex
 
 
 class State(BaseModel, Base):
-    """ State class """
-    __tablename__ = 'states'
-
+    """This is the class for State
+    Attributes:
+        name: input name
+    """
+    __tablename__ = "states"
     name = Column(String(128), nullable=False)
-    if getenv('HBNB_TYPE_STORAGE') == 'db':
-        cities = relationship('City', backref='state', cascade='all, delete-orphan')
+    cities = relationship("City", cascade='all, delete, delete-orphan',
+                          backref="state")
 
-    else:
-        @property
-        def cities(self):
-            '''
-                getter attribute cities that returns the list of City
-                instances with state_id equals to the current State.id
-            '''
-            import models
-            from models.city import City
-
-            list_of_cities = models.storage.all('City').values()
-            cities_by_state = [city for city in cities_by_state if city.state_id == self.id]
-            return cities_by_state
+    @property
+    def cities(self):
+        var = models.storage.all()
+        lista = []
+        result = []
+        for key in var:
+            city = key.replace('.', ' ')
+            city = shlex.split(city)
+            if (city[0] == 'City'):
+                lista.append(var[key])
+        for elem in lista:
+            if (elem.state_id == self.id):
+                result.append(elem)
+        return (result)
