36f02e7be561b67bad241e14b0400243d308452c
Emmanuel Ohwoka <emmyprime2015@gmail.com>
2024-06-26 13:58:52 +0100

    worked on task 6 - 10

--- models/place.py
+++ models/place.py
@@ -1,18 +1,45 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
-from models.base_model import BaseModel
+from os import genv
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey, Integer, Float, Table
+from sqlalchemy.orm import relationship
+import models
+from models.review import Review
+from models import storage
 
 
-class Place(BaseModel):
+link_table = Table(
+      "place_amenity", Base.metadata,
+      Column("place_id", String(60), ForeignKey("places.id"), primary_key=True, nullable=False),
+      Column("amenity_id", String(60), ForeignKey("amenities.id"), primary_key=True, nullable=False),
+  )
+
+
+class Place(BaseModel, Base):
     """ A place to stay """
-    city_id = ""
-    user_id = ""
-    name = ""
-    description = ""
-    number_rooms = 0
-    number_bathrooms = 0
-    max_guest = 0
-    price_by_night = 0
-    latitude = 0.0
-    longitude = 0.0
+    __tablename__ = 'places'
+
+    city_id = Column(String(60), ForeignKey('city_id'), nullable=False)
+    user_id = Column(String(60), ForeignKey('user_id'), nullable=False)
+    name = Column(String(128), nullable=False)
+    description = Column(String(1024))
+    number_rooms = Column(Integer, nullable=False, default=0)
+    number_bathrooms = Column(Integer, nullable=False, default=0)
+    max_guest = Column(Integer, nullable=False, default=0)
+    price_by_night = Column(Integer, nullable=False, default=0)
+    latitude = Column(Float)
+    longitude = Column(Float)
     amenity_ids = []
+
+reviews = relationship("Review", backref="place", cascade="all, delete_orphan")
+amenities = relationship("Amenities", secondary="place_amenities", viewonly=False)
+
+
+if ("HBNB_TYPE_STORAGE") != "db":
+    @property
+    def reviews(self):
+        """Getter attribute reviews that returns the list of Review instances with place_id equals to the current Place.id"""
+
+        all_reviews = storage.all(Review)
+        return [review for review in all_reviews.values() if review.place_id == self.id]
