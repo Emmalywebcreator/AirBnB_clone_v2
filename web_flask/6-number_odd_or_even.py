cb51566ebe3cf47dcbc2b4b054c33aa31ad84de0
Emmanuel Ohwoka <emmyprime2015@gmail.com>
2024-07-29 15:12:02 -0700

    even or odd

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -62,6 +62,5 @@ def number_odd_or_even(n):
     return render_template("6-number_odd_or_even.html", number=n, even_or_odd=even_or_odd)
 
 
-
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=True)
